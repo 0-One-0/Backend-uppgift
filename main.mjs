16e33d48f4c22360454ce3a18f7f60da1940509b
one <one@url.com>
2025-11-27 14:15:33 +0100

    bruno folder

--- main.mjs
+++ main.mjs
@@ -1,11 +1,14 @@
 import express from "express";
 import dotenv from "dotenv";
+import endRouts from "./src/endpoints.mjs";
 dotenv.config();
 import { startDatabase } from "./src/db.mjs";
 startDatabase();
 const app = express();
 app.use(express.json());
 const appPort = Number.parseInt(process.env.APP_PORT);
+
+app.use("/api", endRouts);
 
 app.listen(appPort, () => {
   console.log("Application has started on port: " + appPort);
